103c0ce303a8d1bf20a00277460c6244a0f95d50
Max Lang <17551908+just-max@users.noreply.github.com>
2023-06-06 04:42:04 +0200

    fix output

--- src/ast-check/ast_check_bin.ml
+++ src/ast-check/ast_check_bin.ml
@@ -1,6 +1,7 @@
 open Common.Util
 
 let main (args : string list) : int =
+  (* check all files in (the directories pointed to by) args *)
   let vss =
     args
     |> List.to_seq
@@ -8,12 +9,17 @@ let main (args : string list) : int =
     |> Seq.filter
       (fun p -> Filename.check_suffix p ".ml" && not (Sys.is_directory p))
     |> Seq.map Ast_check.file_violations
-    |> List.of_seq
+    |> Seq.memoize
   in
-  List.iter
+  (* the formatter for violations only works until a new file is parsed:
+     see the comment for [is_quotable_loc] is location.ml, hence the first
+     traversal of the sequence must do the printing *)
+  Seq.iter
     (Format.fprintf Format.std_formatter "%a" Ast_check.format_violations)
     vss ;
-  List.fold_left
+  (* exit with 2 if an exception occurred while parsing, with 1 if there
+     was a non-empty list of violations, and 0 otherwise *)
+  Seq.fold_left
     Result.(fun a -> max a % function Ok [] -> 0 | Ok _ -> 1 | Error _ -> 2)
     0 vss
 
