d38070715ada8c8c8d3d2b1f3ff9a178dbae7828
Max Lang <17551908+just-max@users.noreply.github.com>
2023-07-06 01:43:49 +0200

    add alert for internal library

--- src/common/common.ml
+++ src/common/common.ml
@@ -1,4 +1,6 @@
 (** Functionality common to multiple components. Unstable interface. *)
 
+[@@@alert lp_internal "This module is internal and has an unstable interface!"]
+
 module Util = Util
 module Path_util = Path_util
